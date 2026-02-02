d4cfcc4ef34d2e2d710ba460678e5dcb621475c3
Drake <drake.yang920@gmail.com>
2026-02-02 10:53:02 -0600

    Revert "test"
    This reverts commit 78eaabb7

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,5 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
     System.out.println("This is a test message.");
-    System.out.println("Another update message.");
+    System.out.println("uuga BOGGA");
 }
