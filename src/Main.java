4326890a07f4c1526ab8ab30d23044667922a0d5
Drake <drake.yang920@gmail.com>
2026-02-02 12:22:26 -0600

    Initial exception handle.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    System.out.println("This is a test message.");
-    System.out.println("uuga BOGGA");
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        // initial exception handling structure
+        try {
+            System.out.println("This is a test message.");
+            System.out.println(67);
+        }catch (ArrayIndexOutOfBoundsException ex) {
+            System.err.println("Index out of range of array.");
+        }catch (StringIndexOutOfBoundsException ex) {
+            System.err.println("Index out of range of string.");
+        //} catch (IOException ex) {
+           // System.err.println("Input or output has failed.");
+        }catch (Exception ex) {
+            System.out.println("An error has occurred.");
+            System.err.println(ex.toString());
+        }finally {
+            System.out.println("Game over.");
+        }
+    }
 }
