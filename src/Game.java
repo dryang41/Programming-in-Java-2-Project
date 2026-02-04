910604207536be15fe7d858e532a1b1be82bb7c3
Drake <drake.yang920@gmail.com>
2026-02-04 10:46:56 -0600

    Added log4j and all files associated. Inserted logger object into Game class.

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,11 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Game {
+    final static Logger log = LogManager.getLogger("test");
+
     public static void main(String[] args) {
+        log.debug("Is this working?");
         // initial exception handling structure
         try {
             System.out.println("This is a test message.");
